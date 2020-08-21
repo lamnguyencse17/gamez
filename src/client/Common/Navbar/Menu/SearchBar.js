05e086b59855a1eff60a4a59656177e706a78e8a
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-08-21 13:31:23 +0700

    Search bar placeholder color

--- src/client/Common/Navbar/Menu/SearchBar.js
+++ src/client/Common/Navbar/Menu/SearchBar.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function SearchBar() {
   return (
     <div className="mt-4 lg:mt-0 text-orange-600 lg:w-1/2 md:w-2/3 md:mx-auto sm:w-3/4 sm:mx-0">
-      <input type="text" placeholder="Search an article" className="shadow appearance-none border rounded w-full py-1 px-3"></input>
+      <input type="text" placeholder="Search an article" className="shadow appearance-none border rounded placeholder-orange-600 w-full py-1 px-3"></input>
     </div>
   );
 }
