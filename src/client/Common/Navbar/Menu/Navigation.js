29b891b407a0c2208472bf71c515f1b5657a70d5
lamnguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-08-21 13:28:06 +0700

    Fix color scheme

--- src/client/Common/Navbar/Menu/Navigation.js
+++ src/client/Common/Navbar/Menu/Navigation.js
@@ -5,19 +5,19 @@ export default function Navigation() {
     <>
       <a
         href="#responsive-header"
-        className="block mt-4 lg:inline-block lg:mt-0 text-orange-500 hover:text-white mr-6"
+        className="block mt-4 lg:inline-block lg:mt-0 text-orange-600 hover:text-white mr-6"
       >
         Docs
       </a>
       <a
         href="#responsive-header"
-        className="block mt-4 lg:inline-block lg:mt-0 text-orange-500 hover:text-white mr-6"
+        className="block mt-4 lg:inline-block lg:mt-0 text-orange-600 hover:text-white mr-6"
       >
         Examples
       </a>
       <a
         href="#responsive-header"
-        className="block mt-4 lg:inline-block lg:mt-0 text-orange-500 hover:text-white"
+        className="block mt-4 lg:inline-block lg:mt-0 text-orange-600 hover:text-white"
       >
         Blog
       </a>
--- src/client/Common/Navbar/Menu/SearchBar.js
+++ src/client/Common/Navbar/Menu/SearchBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function SearchBar() {
   return (
-    <div className="mt-4 lg:mt-0 text-teal-200 lg:w-1/2 md:w-2/3 md:mx-auto sm:w-3/4 sm:mx-0">
+    <div className="mt-4 lg:mt-0 text-orange-600 lg:w-1/2 md:w-2/3 md:mx-auto sm:w-3/4 sm:mx-0">
       <input type="text" placeholder="Search an article" className="shadow appearance-none border rounded w-full py-1 px-3"></input>
     </div>
   );
