3dbb091430c1d1d57e721227e343cd71aee0ca41
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2020-08-29 23:02:54 +0700

    Add Description Form

--- frontend/components/Create-Article.js
+++ frontend/components/Create-Article.js
@@ -1,12 +1,14 @@
 import React from "react";
 import EditSpace from "./Editor/EditSpace";
 import TitleForm from "./Editor/TitleForm";
+import DescriptionForm from "./Editor/DescriptionForm";
 
 function CreateArticle(props) {
   return (
     <div className="container mx-auto mt-10">
-      <div className="w-4/5 mb-10 mt-5 mx-auto border-2 border-black p-5">
+      <div className="w-4/5 mb-10 mt-5 mx-auto border-2 border-black p-5 space-y-5">
         <TitleForm />
+        <DescriptionForm />
         <EditSpace />
       </div>
     </div>
---
+++ frontend/components/Editor/DescriptionForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function DescriptionForm(props) {
+  return (
+    <>
+      <input
+        type="text"
+        className="w-full shadow border-2 border-gray-200"
+        placeholder="Description"
+      />
+    </>
+  );
+}
+
+export default DescriptionForm;
